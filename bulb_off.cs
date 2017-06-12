918ef4f6493082f4b3fed25f3ac2d79198398587
basilndonga <basilndonga@gmail.com>
2017-06-12 05:36:35 -0700

    C# code to turn off the bulb

---
+++ bulb_off.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO.Ports;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SerialPort port = new SerialPort("COM5", 9600, Parity.None, 8, StopBits.One); //Change COM5 to your com.
+            port.Open();
+            port.Write("0"); //the data that will be sent to the arduino, change if it is needed.
+            port.Close();
+
+        }
+    }
+}
